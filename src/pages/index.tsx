a33324a2e5b5bc7806067f94e27bab501a4fd38b
Thales Testoni <thalestestoni@gmail.com>
2021-10-23 09:24:38 -0300

    Created project and added initial dependencies

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>worldtrip</title>
+        <meta name="description" content="Informations about continents and cities to travel." />
+      </Head>
+
+      <h1>Hello</h1>
+    </div>
+  )
+}
